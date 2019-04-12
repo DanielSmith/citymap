5ccdb6e6682e7e3704d6d41f94c874f268a24843
Daniel Smith <dls@brooklyn.local>
2019-04-12 17:52:28 -0400

    end of part 1

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,13 +1,73 @@
 <template>
-  <div id="map">I am the map</div>
+  <div id="map"></div>
 </template>
 
 <script>
+import $Scriptjs from 'scriptjs';
+import { DEFAULT_ENCODING } from 'crypto';
 export default {
+  name: "Map",
 
+  data() {
+    return {
+      map: null,
+      marker: null,
+      lastLat: null,
+      lastLng: null,
+
+      // center around the Upper West Side, NYC
+      DEFAULT_LAT: 40.780,
+      DEFAULT_LNG: -73.974
+    }
+  },
+
+  mounted() {
+    $Scriptjs("https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=geometry,places", () => {
+      this.initMap();
+    });
+  },
+
+  methods: {
+    initMap() {
+      this.map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+          lat: this.DEFAULT_LAT,
+          lng: this.DEFAULT_LNG
+        },
+        zoom: 15,
+        gestureHandling: "greedy"
+      });
+
+      google.maps.event.addListener(this.map, "click", (event) => {
+        this.lastLat = event.latLng.lat();
+        this.lastLng = event.latLng.lng();
+
+        this.updateMarker(event.latLng);
+      });
+    },
+
+    updateMarker(latLng) {
+      if (!this.marker) {
+        this.marker = new google.maps.Marker({
+          map: this.map
+        });
+      }
+
+      this.marker.setPosition(latLng);
+    }
+  }
 }
 </script>
 
 <style>
-
+#map {
+  width: 90vw;
+  height: 90vw;
+}
 </style>
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
